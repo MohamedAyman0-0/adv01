409f24237c1f3713ef367bc01034279cfd5a35d8
mohamed ayman <mohamedaymanabdel3ty@gmail.com>
2025-08-19 21:26:23 +0300

    Q1 Optimized Bubble Sort

--- adv01/Program.cs
+++ adv01/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using System;
+
+#region Q1 Optimized Bubble Sort
+
+class BubbleSortOptimized
+{
+    public static void BubbleSort(int[] arr)
+    {
+        int n = arr.Length;
+        bool swapped;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                   
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+           
+            if (!swapped)
+                break;
+        }
+    }
+}
+
+
+#endregion
+#region 
+#endregion
